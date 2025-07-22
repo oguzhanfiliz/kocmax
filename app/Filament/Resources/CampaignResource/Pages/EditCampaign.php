52160f39e9fa58821d8d80700c58a36f6425e85e
Oğuzhan Filiz <oguzhanfiliz3@gmail.com>
2025-07-23 01:12:39 +0300

    eksik policyler eklendi ve geçerli olması için tüm policyler authserviceprovider'a eklendi

---
+++ app/Filament/Resources/CampaignResource/Pages/EditCampaign.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Filament\Resources\CampaignResource\Pages;
+
+use App\Filament\Resources\CampaignResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditCampaign extends EditRecord
+{
+    protected static string $resource = CampaignResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+            
+            Actions\Action::make('duplicate')
+                ->label('Kopyala')
+                ->icon('heroicon-o-document-duplicate')
+                ->color('gray')
+                ->action(function () {
+                    $newCampaign = $this->record->replicate();
+                    $newCampaign->name = $this->record->name . ' (Kopya)';
+                    $newCampaign->start_date = now();
+                    $newCampaign->end_date = now()->addDays(30);
+                    $newCampaign->save();
+                    
+                    // Copy product relationships
+                    $newCampaign->products()->attach($this->record->products->pluck('id'));
+                    
+                    $this->redirect($this->getResource()::getUrl('edit', ['record' => $newCampaign]));
+                }),
+        ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getSavedNotificationTitle(): ?string
+    {
+        return 'Kampanya başarıyla güncellendi!';
+    }
+}
