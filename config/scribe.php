bb1227ba8d8e3c0e52e599389a3f7b2009c27200
Oğuzhan Filiz <oguzhanfiliz3@gmail.com>
2025-08-01 00:28:48 +0300

    özellik: Scribe konfigürasyonunda güncellemeler yapıldı
    - AuthIn sınıfı kaldırıldı ve 'in' parametresi 'bearer' olarak güncellendi
    - API dokümantasyonu için daha basit bir yapı sağlandı

--- config/scribe.php
+++ config/scribe.php
@@ -2,7 +2,6 @@
 
 use Knuckles\Scribe\Extracting\Strategies;
 use Knuckles\Scribe\Config\Defaults;
-use Knuckles\Scribe\Config\AuthIn;
 use function Knuckles\Scribe\Config\{removeStrategies, configureStrategy};
 
 // Only the most common configs are shown. See the https://scribe.knuckles.wtf/laravel/reference/config for all.
@@ -117,7 +116,7 @@ return [
         'default' => false,
 
         // Where is the auth value meant to be sent in a request?
-        'in' => AuthIn::BEARER->value,
+        'in' => 'bearer',
 
         // The name of the auth parameter (e.g. token, key, apiKey) or header (e.g. Authorization, Api-Key).
         'name' => 'key',
